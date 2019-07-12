7fa141a5b913426c0654feaab7332905b674fac4
Curtis241 <copeterson07@gmail.com>
2019-07-11 19:33:21 -0600

    setup file changes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
+sudo apt-get install python3-setuptools python3-dev build-essential virtualenv
+
 rm -rf venv3/
 virtualenv -p python3.6 venv3
 source venv3/bin/activate
+
+cd venv3/bin/
 pip install -r requirements/dev.txt
 deactivate
